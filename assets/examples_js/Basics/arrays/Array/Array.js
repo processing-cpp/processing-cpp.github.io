26b9f97975c77efcd8fdea1c737e068246c04584
Jose Llamas <pep84c@gmail.com>
2026-06-11 15:43:32 -0700

    Add p5.js version of Array example

--- assets/examples_js/Basics/arrays/Array/Array.js
+++ assets/examples_js/Basics/arrays/Array/Array.js
@@ -10,36 +10,37 @@
  * separate ways on the screen.
  */
 
-float* coswave;
+let coswave = [];
 
-void setup() {
-    size(640, 360);
-    coswave = new float[width];
-    for (int i = 0; i < width; i++) {
-        float amount = map(i, 0, width, 0, PI);
-        coswave[i] = abs(cos(amount));
-    }
-    background(255);
-    noLoop();
-}
+p.setup = function() {
+  p.createCanvas(640, 360);
+  for (let i = 0; i < p.width; i++) {
+    let amount = p.map(i, 0, p.width, 0, p.PI);
+    coswave[i] = p.abs(p.cos(amount));
+  }
+  p.background(255);
+  p.noLoop();
+};
 
-void draw() {
-    int y1 = 0;
-    int y2 = height / 3;
-    for (int i = 0; i < width; i++) {
-        stroke(coswave[i] * 255);
-        line(i, y1, i, y2);
-    }
-    y1 = y2;
-    y2 = y1 + y1;
-    for (int i = 0; i < width; i++) {
-        stroke(coswave[i] * 255 / 4);
-        line(i, y1, i, y2);
-    }
-    y1 = y2;
-    y2 = height;
-    for (int i = 0; i < width; i++) {
-        stroke(255 - coswave[i] * 255);
-        line(i, y1, i, y2);
-    }
-}
+p.draw = function() {
+  let y1 = 0;
+  let y2 = p.height / 3;
+  for (let i = 0; i < p.width; i++) {
+    p.stroke(coswave[i] * 255);
+    p.line(i, y1, i, y2);
+  }
+
+  y1 = y2;
+  y2 = y1 + y1;
+  for (let i = 0; i < p.width; i++) {
+    p.stroke(coswave[i] * 255 / 4);
+    p.line(i, y1, i, y2);
+  }
+
+  y1 = y2;
+  y2 = p.height;
+  for (let i = 0; i < p.width; i++) {
+    p.stroke(255 - coswave[i] * 255);
+    p.line(i, y1, i, y2);
+  }
+};
