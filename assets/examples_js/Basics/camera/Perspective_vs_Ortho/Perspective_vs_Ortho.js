bef5d098a6bfbaca85c4898fc3271b73b1357fd8
Jose Llamas <pep84c@gmail.com>
2026-06-11 20:19:05 -0700

    Fix all sidebar links and rewrite reference, about, libraries pages

--- assets/examples_js/Basics/camera/Perspective_vs_Ortho/Perspective_vs_Ortho.js
+++ assets/examples_js/Basics/camera/Perspective_vs_Ortho/Perspective_vs_Ortho.js
@@ -2,27 +2,21 @@ let showPerspective = false;
 
 function setup() {
   createCanvas(600, 360, WEBGL);
-  fill(255);
   noStroke();
 }
 
 function draw() {
-  lights();
   background(0);
+  lights();
 
-  let far = map(mouseX, 0, width, 120, 400);
+  let far = map(mouseX, 0, width, 300, 1000);
 
   if (showPerspective) {
     perspective(PI / 3, width / height, 10, far);
   } else {
-    ortho(
-      -width / 2,
-      width / 2,
-      -height / 2,
-      height / 2,
-      10,
-      far
-    );
+    ortho(-width / 2, width / 2,
+          -height / 2, height / 2,
+          10, far);
   }
 
   rotateX(-PI / 6);
