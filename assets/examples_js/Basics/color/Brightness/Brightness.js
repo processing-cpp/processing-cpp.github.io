a09bd61f915fbaa2d61345c9560d0f6cfebd694b
Jose Llamas <pep84c@gmail.com>
2026-06-11 20:39:31 -0700

    Detect size() and createCanvas() for iframe dimensions

--- assets/examples_js/Basics/color/Brightness/Brightness.js
+++ assets/examples_js/Basics/color/Brightness/Brightness.js
@@ -1,22 +1,27 @@
-int barWidth = 20;
-int lastBar = -1;
+let barWidth = 20;
+let lastBar = -1;
 
-void setup() {
-    size(640, 360);
-    colorMode(HSB, width, 100, height);
-    noStroke();
-    background(0);
+function setup() {
+  createCanvas(640, 360);
+
+  // HSB: hue ranges from 0 to width,
+  // saturation from 0 to 100,
+  // brightness from 0 to height
+  colorMode(HSB, width, 100, height);
+
+  noStroke();
+  background(0);
 }
 
-void draw() {
-    int whichBar = mouseX / barWidth;
+function draw() {
+  let whichBar = floor(mouseX / barWidth);
 
-    if (whichBar != lastBar) {
-        int barX = whichBar * barWidth;
+  if (whichBar !== lastBar) {
+    let barX = whichBar * barWidth;
 
-        fill(barX, 100, mouseY);
-        rect(barX, 0, barWidth, height);
+    fill(barX, 100, mouseY);
+    rect(barX, 0, barWidth, height);
 
-        lastBar = whichBar;
-    }
+    lastBar = whichBar;
+  }
 }
