7edf649767e87e86dc248c1b1a0694c03ae8bbc1
Jose Llamas <pep84c@gmail.com>
2026-06-12 12:13:20 -0700

    Fix Rotate Push Pop example

--- assets/examples_js/Basics/transform/Rotate_Push_Pop/Rotate_Push_Pop.js
+++ assets/examples_js/Basics/transform/Rotate_Push_Pop/Rotate_Push_Pop.js
@@ -10,19 +10,14 @@ function setup() {
 function draw() {
   background(0, 0, 26);
   lights();
-
   for (let i = 0; i < num; i++) {
     let gray = map(i, 0, num - 1, 0, 255);
-
     push();
-
     fill(gray);
     rotateY(a + offset * i);
     rotateX(a / 2 + offset * i);
-    box(200);
-
+    box(100);
     pop();
   }
-
   a += 0.01;
 }
