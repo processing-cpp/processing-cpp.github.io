15a63052614ece431081ae3fdf48f04e9916f23e
Jose Llamas <pep84c@gmail.com>
2026-06-14 13:58:09 -0700

    Fix Rotate Push Pop lighting

--- assets/examples_js/Basics/transform/Rotate_Push_Pop/Rotate_Push_Pop.js
+++ assets/examples_js/Basics/transform/Rotate_Push_Pop/Rotate_Push_Pop.js
@@ -9,16 +9,15 @@ function setup() {
 
 function draw() {
   background(0, 0, 26);
-  ambientLight(60);
-  directionalLight(255, 255, 255, 0, 0, -1);
+  ambientLight(100);
+  directionalLight(255, 255, 255, 0.5, 0.5, -1);
   for (let i = 0; i < num; i++) {
     let gray = map(i, 0, num - 1, 0, 255);
     push();
     fill(gray);
     rotateY(a + offset * i);
     rotateX(a / 2 + offset * i);
-    translate(0, 0, -50 * i);
-    box(100);
+    box(90);
     pop();
   }
   a += 0.01;
