dbe9c934aa76713321ccfd238b14135ffd46377f
Jose Llamas <pep84c@gmail.com>
2026-06-15 10:26:58 -0700

    Move credit below hero text, fix Loading Images to use local asset

--- assets/examples_js/Basics/web/Loading_Images/Loading_Images.js
+++ assets/examples_js/Basics/web/Loading_Images/Loading_Images.js
@@ -6,19 +6,12 @@ function preload() {
 
 function setup() {
   createCanvas(640, 360);
+  noLoop();
 }
 
 function draw() {
   background(0);
-  if (img && img.width > 0) {
-    let h = img.height * (640 / img.width); // scale to fit width
-    for (let i = 0; i < 5; i++) {
-      image(img, 0, h * i, 640, h);
-    }
-  } else {
-    fill(255);
-    textAlign(CENTER);
-    textSize(14);
-    text('Loading...', width/2, height/2);
+  for (let i = 0; i < 5; i++) {
+    image(img, 0, img.height * i);
   }
 }
