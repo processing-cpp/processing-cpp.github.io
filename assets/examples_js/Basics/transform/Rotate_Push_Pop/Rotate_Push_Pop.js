f09741b4e0d503fe85e2603a69f0fc006fc343e5
Jose Llamas <pep84c@gmail.com>
2026-06-14 19:39:50 -0700

    Fix Loading Images example with local asset, fix Rotate Push Pop lighting

--- assets/examples_js/Basics/transform/Rotate_Push_Pop/Rotate_Push_Pop.js
+++ assets/examples_js/Basics/transform/Rotate_Push_Pop/Rotate_Push_Pop.js
@@ -1,17 +1,23 @@
 let a = 0;
-let offset = PI / 24.0;
+let offset;
 let num = 12;
 
 function setup() {
   createCanvas(640, 360, WEBGL);
   noStroke();
+  offset = PI / 24.0;
 }
 
 function draw() {
   background(0, 0, 26);
+  ambientLight(150);
+  directionalLight(255, 255, 255, 0, 0, -1);
+  directionalLight(100, 100, 100, 0, 0, 1);
+
   for (let i = 0; i < num; i++) {
+    let gray = map(i, 0, num - 1, 0, 255);
     push();
-    normalMaterial();
+    fill(gray);
     rotateY(a + offset * i);
     rotateX(a / 2 + offset * i);
     box(90);
--- assets/examples_js/Basics/web/Loading_Images/Loading_Images.js
+++ assets/examples_js/Basics/web/Loading_Images/Loading_Images.js
@@ -1,16 +1,16 @@
 let img;
 
+function preload() {
+  img = loadImage('https://processing-cpp.github.io/assets/data/processing-web.png');
+}
+
 function setup() {
   createCanvas(640, 360);
-
-  img = loadImage("https://processing.org/img/processing-web.png");
-
   noLoop();
 }
 
 function draw() {
   background(0);
-
   if (img) {
     for (let i = 0; i < 5; i++) {
       image(img, 0, img.height * i);
