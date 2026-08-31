e9b1e097b0fb18806ea6bdda8fef2280658e4acd
Jose Llamas <pep84c@gmail.com>
2026-08-30 17:50:21 -0700

    Logo sketch: restore background, fix html

--- assets/logo_sketch.js
+++ assets/logo_sketch.js
@@ -29,7 +29,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmp6l0do3zl.js
+// include: /tmp/tmpemigrr3o.js
 
   if (!Module['expectedDataFileDownloads']) Module['expectedDataFileDownloads'] = 0;
   Module['expectedDataFileDownloads']++;
@@ -48,7 +48,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
         // web worker
         PACKAGE_PATH = encodeURIComponent(location.pathname.substring(0, location.pathname.lastIndexOf('/')) + '/');
       }
-      var PACKAGE_NAME = '/tmp/tmp8r5fs8s1/sketch.data';
+      var PACKAGE_NAME = '/tmp/tmpgyfeo2w5/sketch.data';
       var REMOTE_PACKAGE_BASE = 'sketch.data';
       var REMOTE_PACKAGE_NAME = Module['locateFile'] ? Module['locateFile'](REMOTE_PACKAGE_BASE, '') : REMOTE_PACKAGE_BASE;
       var REMOTE_PACKAGE_SIZE = metadata['remote_package_size'];
@@ -136,9 +136,9 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
         Module['FS_createDataFile'](name, null, data, true, true, true);
         Module['removeRunDependency'](`fp ${name}`);
           }
-          Module['removeRunDependency']('datafile_/tmp/tmp8r5fs8s1/sketch.data');
+          Module['removeRunDependency']('datafile_/tmp/tmpgyfeo2w5/sketch.data');
       }
-      Module['addRunDependency']('datafile_/tmp/tmp8r5fs8s1/sketch.data');
+      Module['addRunDependency']('datafile_/tmp/tmpgyfeo2w5/sketch.data');
 
       if (!Module['preloadResults']) Module['preloadResults'] = {};
 
@@ -161,7 +161,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
   })();
 
-// end include: /tmp/tmp6l0do3zl.js
+// end include: /tmp/tmpemigrr3o.js
 
 
 var arguments_ = [];
